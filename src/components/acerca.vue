922cf6bc61a828f94869c2f9cdc33c1125ffd278
jose909 <rosaleayal19@hotmail.com>
2017-10-09 20:39:33 -0600

    se agregó componente menu-coffee y se agrega una galeria, agrego estilo a Acerca de.

---
+++ src/components/acerca.vue
@@ -0,0 +1,175 @@
+<template>
+  <div>
+    <header>
+        <div class="contenedor">
+          <nav class="menu">
+            <a href="#" id="btn-acerca-de">Acerca de </a>
+            <a href="#" id="btn-menu">Menu </a>
+            <a href="#" id="btn-galeria">Galeria</a>
+            <a href="#" id="btn-ubicacion">Ubicacion</a>
+          </nav>
+    
+          <div class="textos">
+            <h1 class="nombre">Cofe <span>Vue</span></h1>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+          </div>
+        </div>
+    </header>
+
+    <section class="main">
+       <section class="acerca-de" id="acerca-de">
+          <div class="contenedor">
+            <div class="foto">
+                  <img src= "../assets/img/foto10.jpg">
+            </div>
+            <article>
+                <h3>Acerca de</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Nam a convallis libero. Morbi bibendum, tortor ut placerat ornare, dui ligula pellentesque risus, sed tincidunt metus arcu a dui.</p>
+
+                <p> Nam a convallis libero. Morbi bibendum, tortor ut placerat ornare, dui ligula pellentesque risus, sed tincidunt metus arcu a dui. Suspendisse imperdiet suscipit blandit. Pellentesque eget metus sed odio fringilla varius a vitae risus. Donec ac lobortis urna</p>
+             </article>
+          </div>
+        </section>
+    </section>
+    
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'acerca',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style >
+*{
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Open Sans", Arial, Sans-serif;
+  font-size: 13;
+}
+.contenedor {
+  width: 90%;
+  max-width: 1000px;
+  margin: auto;
+  overflow: hidden;
+}
+
+/*----- ----- HEADER ----- -----*/
+header {
+  width: 100%;
+  background: url(../assets/img/foto14.jpg) #281a0c;
+  background-size:cover;
+  background-attachment: fixed;
+  background-position: center 0;
+
+}
+header .contenedor{
+  position: relative;
+  height: 700px;
+}
+
+header .menu {
+  width: 100%;
+  text-align: right;
+}
+
+header .menu a {
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: 300;
+  display: inline-block;
+  margin: 40px 20px;
+  text-decoration: none;
+  position: relative;
+
+}
+
+header .menu a:hover{
+  text-decoration: underline;
+}
+
+header .textos{
+  widows: 100%;
+  color: #fff;
+  position: absolute;
+  top: 50%;
+  margin-top:-52px;
+
+}
+header .textos .nombre{
+  font-size: 50px;
+  font-weight: 600;
+}
+
+header .textos .nombre span{
+  font-weight: 300;
+}
+
+header .textos {
+  font-size: 25px;
+  font-weight: 300;
+  font-style: italic;
+}
+
+/* ----- ---- MAIN ---- ---- */
+
+
+.main .acerca-de .contenedor{
+  overflow: visible;
+  position: relative;
+}
+
+.main .acerca-de .foto {
+  width: 80%;
+  position: absolute;
+  left: 0;
+  top:-50px;
+  z-index: 0;
+  box-shadow: 0px 0px 15px rgba(0.0.0,.35);
+}
+
+.main .acerca-de .foto {
+  vertical-align: top;
+  width: 100%;
+}
+.main .acerca-de article {
+  background: #fff;
+  padding: 40px;
+  width: 50%;
+  position: relative;
+  top: 150px;
+  z-index: 1;
+  float: right;
+  box-shadow: 0px 0px 15px rgba(0.0.0,.35);
+
+}
+
+.main .acerca-de .article h3 {
+  font-size: 50px;
+  font-weight: 300;
+  margin-bottom: 40px;
+}
+
+.main .acerca-de article p {
+  line-height: 28px;
+  margin-bottom: 15px
+}
+
+
+
+
+</style>
---
+++ src/components/menu_caffe.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <section class="menu-platillo">
+      <div class="contenedor">
+        <h3 class="titulo" id="platillos">Menu</h3>
+        <div class="contenedor-menu">
+         <div class="contenedor-menu2">
+            <article>
+              <p class="categoria">De Comer</p>
+              <div class="platillo">
+                <p class="nombre">Sed sageittis nisl dictum</p>
+                <p class="precio">$15</p>
+                <p class="descripcion">Praesent gravia, augu sit amet gignissim rutrum, enim telus suscit risus.</p>
+              </div>
+
+               <div class="platillo">
+                  <p class="nombre">Sed sageittis nisl dictum</p>
+                  <p class="precio">$15</p>
+                  <p class="descripcion">Praesent gravia, augu sit amet gignissim rutrum, enim telus suscit risus.</p>
+              </div>
+
+              <div class="platillo">
+                <p class="nombre">Sed sageittis nisl dictum</p>
+                <p class="precio">$15</p>
+                <p class="descripcion">Praesent gravia, augu sit amet gignissim rutrum, enim telus suscit risus.</p>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="galeria" id="galeria">
+      <div class="foto">
+        <img src="../assets/img/foto1.jpg" alt="">
+      </div>
+      <div class="foto">
+        <img src="../assets/img/foto2.jpg" alt="">
+      </div>
+      <div class="foto">
+        <img src="../assets/img/foto3.jpg" alt="">
+      </div>
+      <div class="foto">
+        <img src="../assets/img/foto4.jpg" alt="">
+      </div>
+      <div class="foto">
+        <img src="../assets/img/foto5.jpg" alt="">
+      </div>
+      <div class="foto">
+        <img src="../assets/img/foto6.jpg" alt="">
+      </div>
+      <div class="foto">
+        <img src="../assets/img/foto7.jpg" alt="">
+      </div>
+      <div class="foto">
+        <img src="../assets/img/foto8.jpg" alt="">
+      </div>
+     
+      
+     
+     
+
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'menu_caffe',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Open Sans", Arial, Sans-serif;
+  font-size: 16;
+}
+
+.contenedor {
+  width: 90%;
+  max-width: 1000px;
+  margin: auto;
+  overflow: hidden;
+}
+.menu-platillo {
+  padding-top: 570px;
+  margin-bottom: 50px;
+}
+
+.menu-platillo .titulo{
+  font-size: 40px;
+  font-weight: 300;
+  text-align: center;
+  margin-bottom: 40px;
+}
+
+.menu-platillo .contenedor-menu,
+.menu-platillo .contenedor-menu2 {
+  border:1px solid #000;
+  background: transparent;
+  padding: 10px;
+  overflow: hidden;
+}
+</style>
